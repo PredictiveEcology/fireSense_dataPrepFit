57b0e847a4567a4e03a1ef51d0dac5438a6530a6
Eliot McIntire <eliotmcintire@gmail.com>
2025-04-15 16:38:20 -0700

    loadOrder

--- fireSense_dataPrepFit.R
+++ fireSense_dataPrepFit.R
@@ -12,7 +12,9 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = deparse(list("README.md", "fireSense_dataPrepFit.Rmd")),
-  loadOrder = list(after = c("Biomass_borealDataPrep", "Biomass_speciesParameters")),
+  loadOrder = list(before = c("Biomass_speciesData", "Biomass_borealDataPrep", "Biomass_speciesParameters"),
+                   after = c("canClimateData")),
+  # list(after = c("Biomass_borealDataPrep", "Biomass_speciesParameters")),
   reqdPkgs = list("data.table", "fastDummies", "reproducible", # studyAreaName
                   "PredictiveEcology/fireSenseUtils@development (>= 0.0.5.9084)",
                   "ggplot2", "parallel", "purrr", "raster", "sf", "sp",
