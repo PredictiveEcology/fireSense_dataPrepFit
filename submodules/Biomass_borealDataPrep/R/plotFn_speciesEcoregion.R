20f31f7478b17fdfb44dd4f2a3ba27332e173aa4
Ian Eddy <ianmseddy@gmail.com>
2025-04-15 12:59:28 -0700

    rm not-git submodule

--- submodules/Biomass_borealDataPrep/R/plotFn_speciesEcoregion.R
+++
@@ -1,8 +0,0 @@
-plotFn_speciesEcoregion <- function(stk, SEtype) {
-  ggplot() +
-    tidyterra::geom_spatraster(data = stk) +
-    facet_wrap(~ lyr) +
-    scale_fill_distiller(palette = "YlGnBu", direction = 1, na.value = "transparent") +
-    theme_bw() +
-    ggtitle(SEtype)
-}
